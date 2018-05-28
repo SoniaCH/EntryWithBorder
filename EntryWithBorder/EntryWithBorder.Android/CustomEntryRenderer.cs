779164387326cdc9fe6264280259ec26d90bb43b
SoniaChnini <chnini.sonia@gmail.com>
2018-05-28 13:20:34 +0100

    Add project files.

---
+++ EntryWithBorder/EntryWithBorder.Android/CustomEntryRenderer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using EntryWithBorder.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(CustomEntryRenderer))]
+namespace EntryWithBorder.Droid
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        public CustomEntryRenderer(Context context) : base(context)
+        {
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                Control.Background = this.Resources.GetDrawable(Resource.Drawable.Border);
+                Control.SetPadding(10, 10, 10, 3);
+            }
+        }
+    }
+}
---
+++ EntryWithBorder/EntryWithBorder.iOS/CustomEntryRenderer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EntryWithBorder.iOS;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(CustomEntryRenderer))]
+namespace EntryWithBorder.iOS
+{
+    public class CustomEntryRenderer: EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                // Create a custom border with square corners
+                Control.BorderStyle = UITextBorderStyle.None;
+                Control.Layer.CornerRadius = 0;
+                Control.Layer.BorderWidth = .5f;
+                Control.Layer.BorderColor = UIColor.FromRGB(200, 200, 200).CGColor;
+            }
+        }
+    }
+}
